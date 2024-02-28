c1aa8dbd94d29c63062bf3df70b58cb3e4502450
Yuri140 <yurisousa.carneiro2006@gmail.com>
2024-02-27 21:11:15 -0300

    voltei da tomada de café

--- app/src/main/java/br/com/etecia/appprodutos/ProdutosAdapter.java
+++ app/src/main/java/br/com/etecia/appprodutos/ProdutosAdapter.java
@@ -18,65 +18,63 @@ import java.util.List;
 public class ProdutosAdapter extends RecyclerView.Adapter<ProdutosAdapter.ViewHolder> {
 
     private Context context;
-    private List<Produtos> ListaProdutos;
+    private List<Produtos> listaProdutos;
 
     //construtor da classe adapter
-
-
     public ProdutosAdapter(Context context, List<Produtos> listaProdutos) {
         this.context = context;
-        ListaProdutos = listaProdutos;
+        this.listaProdutos = listaProdutos;
     }
 
     @NonNull
     @Override
     public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-        return null;
-
-        //instanciar modelo para receber os dados
-
+        //instanciar o modelo para receber os dados
         View view;
 
         LayoutInflater inflater = LayoutInflater.from(context);
-
         view = inflater.inflate(R.layout.modelo_produtos, parent, false);
 
         return new ViewHolder(view);
     }
 
+
     @Override
     public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
-        holder.txtProduto.setText(ListaProdutos.get(position).getDescricao());
-        holder.imgProduto.setImageResource(ListaProdutos.get(position).getImagem());
-        //clique no cardview do modelo
+        holder.txtProduto.setText(listaProdutos.get(position).getDescricao());
+        holder.imgProduto.setImageResource(listaProdutos.get(position).getImagem());
 
+        //clique no cardview do modelo
         holder.cardProduto.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Toast.makeText(context,"TESTE", Toast.LENGTH_SHORT).show();
+                Toast.makeText(context,"Teste",Toast.LENGTH_SHORT).show();
             }
         });
-    }
 
+    }
 
     @Override
     public int getItemCount() {
-        return ListaProdutos.size();
-
+        return listaProdutos.size();
     }
 
     public class ViewHolder extends RecyclerView.ViewHolder {
-        //declarando variaveis que irão representar o xml no java
+        //declarando as variáveis que irão representar o xml no java
         ImageView imgProduto;
         TextView txtProduto;
         MaterialCardView cardProduto;
 
         public ViewHolder(@NonNull View itemView) {
             super(itemView);
+
             imgProduto = itemView.findViewById(R.id.idImgProduto);
             txtProduto = itemView.findViewById(R.id.txtDescrProduto);
             cardProduto = itemView.findViewById(R.id.idCardProduto);
+
+
         }
     }
+
 
 }
